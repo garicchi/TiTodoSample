946381db85ff426b1e25e11c3d74ee73f46e401b
garicchi <xgaricchi@gmail.com>
2015-07-19 22:53:09 +0900

    test

---
+++ test.js
@@ -0,0 +1,24 @@
+/*describe('Android browser.js', function(){
+	var browserjs = path.join(__dirname, 'Resources', 'android', 'alloy', 'controllers', 'browser.js'),
+		context = slag(browserjs, {
+			titanium: TITANIUM,
+			alloy: ALLOY,
+			platform: 'android',
+			module: {
+				Alloy: Alloy.android
+			}
+		});
+
+	it('should does not throw exception in the main Controller', function(){
+		assert.doesNotThrow(function(){
+			context.Controller();
+		});
+	});
+
+	it('should does not throw exception in the doRender', function(){
+		assert.doesNotThrow(function(){
+			context.doRender();
+		});
+	});
+});
+*/
